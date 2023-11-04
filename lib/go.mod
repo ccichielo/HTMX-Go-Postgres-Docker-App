6951f2d988ae1c0e41e6ded82219c6da1c1b8cb4
ccichielo <chris.cichielo@gmail.com>
2023-11-03 20:52:50 -0400

    Able to render picture sourced from database (#1)
    * bla
    
    * Rendered image sourced from Postgres successfully

---
+++ lib/go.mod
@@ -0,0 +1,5 @@
+module example.com/lib
+
+go 1.21.3
+
+require github.com/lib/pq v1.10.9
--- main.go
+++ main.go
@@ -1,9 +1,52 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+
+	"example.com/lib"
 )
 
-func main(){
-	fmt.Println("Hello world!")
+func WebHandler(w http.ResponseWriter, r *http.Request) {
+	lib.TestDbConnection()
+	imgByte := lib.GetImageBytesById(1)
+
+	/*img, err := jpeg.Decode(bytes.NewReader(imgByte))
+	if err != nil {
+		log.Fatalln(err)
+	}*/
+
+	// Generate HTML using a template
+	htmlTemplate := `<html>
+        <body>
+            <img src="data:image/{{.ImageType}};base64,{{.ImageData}}" alt="Decoded Image">
+        </body>
+    </html>`
+
+	data := struct {
+		ImageType string
+		ImageData string
+	}{
+		ImageType: "jpeg", // or "png"
+		ImageData: base64.StdEncoding.EncodeToString(imgByte),
+	}
+
+	tmpl, err := template.New("html").Parse(htmlTemplate)
+	if err != nil {
+		fmt.Println("Error parsing HTML template:", err)
+		return
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		fmt.Println("Error executing HTML template:", err)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", WebHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
